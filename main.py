685e58c7b780e8b6c35efe3379da0598190a0b89
Alec Dewulf <alec@cisudbury.ca>
2021-06-22 16:55:35 -0400

    comments and clean-up

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 from matplotlib import pyplot as plt
 import math
 
-"""assume meteroite is a sphere"""
+"""
+Assumptions
+- Meteroite is a sphere
+- Meteroite has uniform density
+- All lost energy goes into heating the meteroid (not the air)
+
+
+All units are SI
+"""
+
 
 # acceleration due to gravity
 acceleration = 9.81
@@ -24,35 +33,32 @@ def calc_g(position: int):
 
 
 def calc_mass(temp: int, t: int):
-    """Using the Knudsen-Langmuir equation"""
+    """Using the Knudsen-Langmuir equation to calculate mass loss due to ablation"""
     p_vap = 10**(12.509-20014/temp)
 
     return -4 * math.pi * 0.001**2*p_vap*math.sqrt(1.4*10**-8/2*math.pi*1.38 * 10**-23 * temp) * t
 
 def calc_k(mass: int):
+    
     return DRAG * DENSITY*A/(2*mass)
 
     
 
 
 
-# 20 km/s 20 000 m /s
+# 200 m /s
 v = 200
-
-# atmosphere thickness
-THICKNESS = 10**5
-
 epsilon = 0.01
 time = 0
 z = THICKNESS
 
-
-
+# velocity-time graph
 v_t = plt.figure()
 axes = v_t.add_subplot(111)
 axes.set_ylabel("Position")
 axes.set_xlabel("Time")
 
+# mass-time graph
 m_t = plt.figure()
 axes2 = m_t.add_subplot(111)
 axes2.set_ylabel("Mass")
@@ -60,28 +66,30 @@ axes2.set_xlabel("Time")
 
 a, b, c, t = [], [], [], []
 
+# initial mass as a product of volume and density 
 mass = 4/3 * math.pi* 10000**3
 temp = 0
 
 while z > 0:
     
     time += epsilon
+    # update graviational acceleration
     acceleration = calc_g(z)
 
-    k = DRAG * DENSITY*A/(2*mass)
-    #print(k)
+    # udpate the constant k
+    k = calc_k(mass)
+    
     # update position
     z -= v*epsilon + 1/2 *(acceleration - k*v**2) * epsilon**2
     # update velocity
     v += (acceleration - k*v**2)*epsilon
 
     # melting point of iron is 1538 deg C
+    # increase temperature until the melting point is reached
     if (temp < 1538):
         temp += DRAG * DENSITY * A/2 * v**2
     mass += calc_mass(temp, time)
-    #print(calc_mass(temp, time))
-
-    #print(z)
+   
     # save variables
     a.append(z)
     b.append(v)
