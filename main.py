496cafc1823251d98e7b877b79042add23a20636
Alec Dewulf <56365692+aldew5@users.noreply.github.com>
2021-08-27 12:20:58 -0400

    clean up

--- main.py
+++ main.py
@@ -23,19 +23,28 @@ THICKNESS = 480000
 DENSITY = 8000 #kg/m^3
 DRAG = 0.5
 
-
+R = 6500 #m
 area = math.pi * 7500 **2
-R = 7500 #m
 
-def calc_radius(temp: int, r: float):
+
+def calc_radius(temp: int, r):
     """ri = ro - 0.3 * K/sigmaT^3"""
+
+    # nothing has ablated 
     if (temp == 0):
         return r
-    # subtract the ablated section from the radius
-    ri = r - 0.3 * 86/((5.67 * 10**-8) * temp**3)
-    #print("TERM:DLKJF:", 0.3 * 86/((5.67 * 10**-8) * temp**3))
-    #print("INNER", ri)
-    return r - (r-ri)
+    # calculate the inner radius
+    ri = r - 0.3 * 86* 10**8 /(5.67 * temp**3)
+   # print("IN", temp**3 * 5.67)
+    #print("TEMP", temp)
+    #print("IN", ri, - 0.3 * 86/((5.67 * 10**-8) * temp**3))
+
+    # new radius
+    #print("RETURNING", ri)
+    if (ri < 0):
+        return r
+    
+    return ri
     
 def calc_g(position: int):
     """g = GM/r^2"""
@@ -47,15 +56,15 @@ def calc_g(position: int):
     return 6.67 * 10**-11 * m_planet / r**2
 
 
-def calc_mass(temp: int, t: int):
+def calc_mass(temp: int, t: int, r_o: int):
     """Using the Knudsen-Langmuir equation to calculate mass loss due to ablation"""
     #print(temp)
     p_vap = 10**(12.509-20014/temp)
 
-    return -4 * math.pi * 0.001**2*p_vap*math.sqrt(1.4*10**-8/2*math.pi*1.38 * 10**-23 * temp) * t
+    return -4 * math.pi * r_o**2*p_vap*math.sqrt(1.4*10**-8/2*math.pi*1.38 * 10**-23 * temp) * t
 
 def calc_k(mass: int, area: float):
-    
+    #print("K", DRAG, DENSITY, area, 2 * mass)
     return DRAG * DENSITY*area/(2*mass)
 
     
@@ -63,7 +72,7 @@ def calc_k(mass: int, area: float):
 
 
 # 200 m /s
-v = 200
+v = 1
 epsilon = 0.1
 time = 0
 z = THICKNESS
@@ -86,6 +95,8 @@ a, b, c, t = [], [], [], []
 mass = 4/3 * math.pi* 10000**3
 temp = 0
 
+radius = R
+
 while z > 0:
     
     time += epsilon
@@ -95,25 +106,31 @@ while z > 0:
 
     # update the area
     radius = calc_radius(temp, R)
+    #print("HERE", radius, prev_radius)
     area = math.pi * radius **2
+    #print("AREA", math.pi, radius)
 
     # udpate the constant k
     k = calc_k(mass, area)
     
     # update position
-    #print("TERM 1", v*epsilon, "TERM 2: ", 1/2 *(acceleration - k*v**2) * epsilon**2)
     z -= (v*epsilon + (acceleration - k*v**2)/(4*mass) * epsilon**2)
     # update velocity
     v += (acceleration - (k*v**2)/mass)*epsilon
-    #print("V",v)
+    #print("V", k)
 
-    #print("V and Z", v, z)
-
-    # melting point of iron is 1538 deg C
+    # melting point of taeunite is 1500 deg C
     # increase temperature until the melting point is reached
     if (temp < 1500):
-        temp += DRAG * DENSITY * area/2 * v**2
-    mass += calc_mass(temp, time)
+        #print("Calc", drag *
+        #print("THICNKNESS", THICKNESS, z)
+        #print(THICKNESS - z)
+        temp += DRAG * DENSITY * area/2 * (THICKNESS-z) * v**2/(450*mass)
+        #print(DRAG, (THICKNESS-z), 450*mass)
+        #print('after temp', temp)
+    mass += calc_mass(temp, time, radius)
+
+    #print(mass, v, z, acceleration, area)
    
     # save variables
     a.append(z)
@@ -121,10 +138,8 @@ while z > 0:
     c.append(mass)
     t.append(time)
 
-    #print('z:', z)
-
-
-
+print(mass, v)
+print("KINETIC ENERGY", mass * v**2/2)
 axes.plot(t, a)
 axes2.plot(t, c)
 plt.show()
